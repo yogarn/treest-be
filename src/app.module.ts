0105748ec8591d8b4db4292fadfa34904f3f978a
Yoga Raditya <radityanalaa@gmail.com>
2024-05-05 07:47:11 +0700

    chore(prisma): Add Prisma module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './database/prisma/prisma.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
+  imports: [PrismaModule],
 })
 export class AppModule {}
