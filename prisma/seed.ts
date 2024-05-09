615e926c71b6fe2e9dc37dc5adfd18b15eba8445
Yoga Raditya <radityanalaa@gmail.com>
2024-05-09 17:42:29 +0700

    chore(prisma): Add news seeder

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,7 +4,7 @@ import * as bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
 async function main() {
-  // first solar
+  // seeding company
   const companyList = [
     'First Solar',
     'Vestas Wind System',
@@ -14,14 +14,7 @@ async function main() {
     'Waste Management',
   ];
 
-  const stockSymbols = [
-    'FSLR',
-    'VWS',
-    'ORA',
-    'TSLA',
-    'BEP',
-    'WM'
-  ]
+  const stockSymbols = ['FSLR', 'VWS', 'ORA', 'TSLA', 'BEP', 'WM'];
 
   for (var i = 0; i < companyList.length; i++) {
     console.log(`seeding ${companyList[i]}...`);
@@ -73,6 +66,160 @@ async function main() {
             },
           ],
         },
+        portfolios: {
+          create: [
+            {
+              title: faker.commerce.product(),
+              detail: faker.word.words(5),
+            },
+            {
+              title: faker.commerce.product(),
+              detail: faker.word.words(5),
+            },
+            {
+              title: faker.commerce.product(),
+              detail: faker.word.words(5),
+            },
+            {
+              title: faker.commerce.product(),
+              detail: faker.word.words(5),
+            },
+          ],
+        },
+        news: {
+          create: [
+            {
+              title: faker.word.words(5),
+              body: faker.word.words(100),
+              creator: {
+                create: {
+                  id: faker.string.uuid(),
+                  username: faker.internet.userName(),
+                  password: await bcrypt.hash('root', 10),
+                  email: faker.internet.email(),
+                  name: `${faker.person.firstName()} ${faker.person.lastName()}`,
+                },
+              },
+              comments: {
+                create: [
+                  {
+                    content: faker.word.words(50),
+                    user: {
+                      create: {
+                        id: faker.string.uuid(),
+                        username: faker.internet.userName(),
+                        password: await bcrypt.hash('root', 10),
+                        email: faker.internet.email(),
+                        name: `${faker.person.firstName()} ${faker.person.lastName()}`,
+                      },
+                    },
+                  },
+                  {
+                    content: faker.word.words(50),
+                    user: {
+                      create: {
+                        id: faker.string.uuid(),
+                        username: faker.internet.userName(),
+                        password: await bcrypt.hash('root', 10),
+                        email: faker.internet.email(),
+                        name: `${faker.person.firstName()} ${faker.person.lastName()}`,
+                      },
+                    },
+                  },
+                  {
+                    content: faker.word.words(50),
+                    user: {
+                      create: {
+                        id: faker.string.uuid(),
+                        username: faker.internet.userName(),
+                        password: await bcrypt.hash('root', 10),
+                        email: faker.internet.email(),
+                        name: `${faker.person.firstName()} ${faker.person.lastName()}`,
+                      },
+                    },
+                  },
+                  {
+                    content: faker.word.words(50),
+                    user: {
+                      create: {
+                        id: faker.string.uuid(),
+                        username: faker.internet.userName(),
+                        password: await bcrypt.hash('root', 10),
+                        email: faker.internet.email(),
+                        name: `${faker.person.firstName()} ${faker.person.lastName()}`,
+                      },
+                    },
+                  },
+                ],
+              },
+            },
+            {
+              title: faker.word.words(5),
+              body: faker.word.words(100),
+              creator: {
+                create: {
+                  id: faker.string.uuid(),
+                  username: faker.internet.userName(),
+                  password: await bcrypt.hash('root', 10),
+                  email: faker.internet.email(),
+                  name: `${faker.person.firstName()} ${faker.person.lastName()}`,
+                },
+              },
+              comments: {
+                create: [
+                  {
+                    content: faker.word.words(50),
+                    user: {
+                      create: {
+                        id: faker.string.uuid(),
+                        username: faker.internet.userName(),
+                        password: await bcrypt.hash('root', 10),
+                        email: faker.internet.email(),
+                        name: `${faker.person.firstName()} ${faker.person.lastName()}`,
+                      },
+                    },
+                  },
+                  {
+                    content: faker.word.words(50),
+                    user: {
+                      create: {
+                        id: faker.string.uuid(),
+                        username: faker.internet.userName(),
+                        password: await bcrypt.hash('root', 10),
+                        email: faker.internet.email(),
+                        name: `${faker.person.firstName()} ${faker.person.lastName()}`,
+                      },
+                    },
+                  },
+                  {
+                    content: faker.word.words(50),
+                    user: {
+                      create: {
+                        id: faker.string.uuid(),
+                        username: faker.internet.userName(),
+                        password: await bcrypt.hash('root', 10),
+                        email: faker.internet.email(),
+                        name: `${faker.person.firstName()} ${faker.person.lastName()}`,
+                      },
+                    },
+                  },
+                  {
+                    content: faker.word.words(50),
+                    user: {
+                      create: {
+                        id: faker.string.uuid(),
+                        username: faker.internet.userName(),
+                        password: await bcrypt.hash('root', 10),
+                        email: faker.internet.email(),
+                        name: `${faker.person.firstName()} ${faker.person.lastName()}`,
+                      },
+                    },
+                  },
+                ],
+              },
+            },
+          ],
+        },
       },
     });
   }
@@ -92,6 +239,20 @@ async function main() {
       },
     });
   }
+
+  // seeding admin user
+  console.log(`seeding admin...`);
+  await prisma.user.upsert({
+    where: { email: 'admin@root' },
+    update: {},
+    create: {
+      username: 'admin',
+      password: await bcrypt.hash('root', 10),
+      email: 'admin@root',
+      name: 'Administrator',
+      isAdmin: true,
+    },
+  });
 }
 main()
   .then(async () => {
